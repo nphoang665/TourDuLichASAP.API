3b6448b4044b6b92b904bcbb982a2dff71cf67d5
nphoang665 <phihoang665@gmail.com>
2024-03-26 16:08:27 +0700

    #Auth google

---
+++ TourDuLichASAP.API/Models/DTO/GoogleLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TourDuLichASAP.API.Models.DTO
+{
+    public class GoogleLoginDto
+    {
+        public string Email { get; set; }
+        public string IdToken {  get; set; }
+    }
+}
