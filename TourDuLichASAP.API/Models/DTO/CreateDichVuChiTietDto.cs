0cad9a8f40640d119fae66849219796d77c6c8d2
thanh1709 <thanhvtp2004@gmail.com>
2024-03-27 18:03:58 +0700

    _api Dich vu chi tiet

---
+++ TourDuLichASAP.API/Models/DTO/CreateDichVuChiTietDto.cs
@@ -0,0 +1,16 @@
+﻿using TourDuLichASAP.API.Models.Domain;
+
+namespace TourDuLichASAP.API.Models.DTO
+{
+    public class CreateDichVuChiTietDto
+    {
+        public string IdDichVuChiTiet { get; set; }
+        public string IdDichVu { get; set; }
+        public string IdKhachHang { get; set; }
+        public string IdDatTour { get; set; }
+        public string IdNhanVien { get; set; }
+        public DateTime ThoiGianDichVu { get; set; }
+        public int SoLuong { get; set; }
+ 
+    }
+}
