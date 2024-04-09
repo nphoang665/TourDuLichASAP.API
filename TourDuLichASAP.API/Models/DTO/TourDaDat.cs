e982e06387499f37d1a8d53d88b09711943d3a5c
thanh1709 <thanhvtp2004@gmail.com>
2024-04-09 19:46:31 +0700

    _chart API

---
+++ TourDuLichASAP.API/Models/DTO/TourDaDat.cs
@@ -0,0 +1,9 @@
+﻿namespace TourDuLichASAP.API.Models.DTO
+{
+    public class TourDaDat
+    {
+        public string TenTour { get; set; }
+        public int TongTien { get; set; }
+        public int SoLuong {  get; set; }   
+    }
+}
